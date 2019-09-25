b59b117237c86bf91940f09a93811cd4167b8cf2
rafael.ulic <rafael.ulic@gmail.com>
2019-09-25 14:26:08 +0800

    Updates

---
+++ precache-manifest.a36b2ed8afbc112bf71f5a2d2c3dd46f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0f78629cf6cd730df0bb3d70a02b2f",
+    "url": "/gitportfolio/index.html"
+  },
+  {
+    "revision": "fda5cca6fb2a5891e09b",
+    "url": "/gitportfolio/static/css/main.2b05b760.chunk.css"
+  },
+  {
+    "revision": "3785e943365befc08c5d",
+    "url": "/gitportfolio/static/js/2.a6488915.chunk.js"
+  },
+  {
+    "revision": "fda5cca6fb2a5891e09b",
+    "url": "/gitportfolio/static/js/main.edfbfb74.chunk.js"
+  },
+  {
+    "revision": "3909ba957b96e2ca19d1",
+    "url": "/gitportfolio/static/js/runtime~main.c40e020e.js"
+  },
+  {
+    "revision": "e01a06de32d19130e1a1be5f1aafc5e0",
+    "url": "/gitportfolio/static/media/A.e01a06de.jpg"
+  },
+  {
+    "revision": "873445185f6a775e78a638eab083aea0",
+    "url": "/gitportfolio/static/media/B.87344518.jpg"
+  },
+  {
+    "revision": "2baf4d015052da74fcbf78765747f0c8",
+    "url": "/gitportfolio/static/media/C.2baf4d01.jpg"
+  },
+  {
+    "revision": "cf1217790737fcb57a67b527f0ebacc3",
+    "url": "/gitportfolio/static/media/ciitbg.cf121779.jpg"
+  },
+  {
+    "revision": "3404ebc0b66f11f5482eb44c9301c9e4",
+    "url": "/gitportfolio/static/media/juancashbg.3404ebc0.jpg"
+  },
+  {
+    "revision": "0cd0e3d9dd8e0ab0d465c8aa6be46afc",
+    "url": "/gitportfolio/static/media/juanexbg.0cd0e3d9.jpg"
+  },
+  {
+    "revision": "bfe6973c342906603c5f081dbf29a48d",
+    "url": "/gitportfolio/static/media/rafportrait.bfe6973c.jpg"
+  },
+  {
+    "revision": "c281c6998766fc354425a8b09fdcb911",
+    "url": "/gitportfolio/static/media/scubedbg.c281c699.jpg"
+  }
+]);
